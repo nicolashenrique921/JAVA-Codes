1d58ccce88c3267b52bd0c09e78928f51fa49b52
nicolashenrique921 <nicolashenrique921@gmail.com>
2023-06-27 19:02:38 -0300

    Exercicio_de_identacao_boletimEstudantil

---
+++ BoletimEstudantil_Exercicio/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int mediaFinal = 7;
+         
+        if (mediaFinal < 6)
+            System.out.println("REPROVADO");
+        else if (mediaFinal == 6)
+            System.out.println("PROVA MINERVA");
+        else
+            System.out.println("APROVADO");
+    }
+}
