ac82c60d23112112208275e7ca36c48adc8953f5
nicolashenrique921 <nicolashenrique921@gmail.com>
2023-08-13 14:53:28 -0300

    Graos em tabuleiro de xadres exercicio01

---
+++ Meus-primeiros-projetos-java/edu/Nicolas/primeirosPassos/Programacao_orientada_a_objetos/Graos tabuleiro de xadres exercicio01/src/GraoRei.java
@@ -0,0 +1,16 @@
+import java.math.BigDecimal;
+
+public class GraoRei {
+    public static void main(String[] args) throws Exception {
+        int quadro = 64;
+        BigDecimal dois = new BigDecimal(2);
+        BigDecimal grao = new BigDecimal(1);
+        BigDecimal graoTotal = new BigDecimal(0);
+
+        for (int i = 1; i < quadro; i++){
+            grao = grao.multiply(dois);
+            graoTotal = graoTotal.add(grao);
+        }
+        System.out.println("O total de grão esperados é " + graoTotal);
+    }
+}
